3b59b6908b0a37ca58d61b065da48d120f44166b
henrique <luiz.isbaes@feltracoepertusatti.com.br>
2025-03-18 11:39:58 -0300

    New way to set content inside component using Children props.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hooks from "./components/Hooks";
 import List from "./components/List";
 import RenderCond from "./components/RenderCond";
 import Fragment from "./components/Fragment";
+import Container from "./components/Container";
 function App() {
   return (
     <div className="App">
@@ -15,6 +16,9 @@ function App() {
         <List />
         <RenderCond x={5}/>
         <Fragment />
+        <Container>
+          <h1>Esté é conteudo pelo filho do container</h1>
+        </Container>
     </div>
   );
 }
