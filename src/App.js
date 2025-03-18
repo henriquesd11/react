ce688b2e316a460360bd504a26359c333614045c
henrique <luiz.isbaes@feltracoepertusatti.com.br>
2025-03-18 11:10:49 -0300

    Hooks and useEffect exemples

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import FirstComponent from "./components/FirstComponent";
 import Images from "./components/Images";
+import Hooks from "./components/Hooks";
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
         <FirstComponent/>
         <Images />
+        <Hooks />
     </div>
   );
 }
--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+
 function Hooks() {
+    let idade: number = 30;
+    const [novaIdade, setNovaIdade] = useState(40);
+
+    function changeAge()
+    {
+        idade = 31;
+    }
+
+    function changeNewAge()
+    {
+        setNovaIdade(novaIdade + 1)
+    }
+
+    // exemplo de useEffect, geralmente usado para algo que deve ser executado ao renderizar a pagina, exemplo buscar dados de API.
+    useEffect(() => {
+        console.log('Testando useEffect')
+    })
     return (
         <div>
-
+            <p>Idade: {novaIdade}</p>
+            <button onClick={changeAge}>Mudar Idade</button>
+            <button onClick={changeNewAge}>Change new Age</button>
         </div>
     );
 }
